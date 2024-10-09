5d2de5df669406fa54cddfd0ef90c02473e2ef64
becrew <fiqru.s@gmail.com>
2024-10-10 02:08:04 +0700

    new

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-import app from './index';
+import app from './src/index';
 
 const PORT = process.env.PORT || 3000;
 
